d930f0f9d05f5b2cf3e24cd0e98c899377db2f32
kaibaooo <kailunhuang0904@gmail.com>
2019-07-04 10:55:52 +0800

    update dir

---
+++ UVA/UVA00272_TEX Quotes.cpp
@@ -0,0 +1,27 @@
+
+/*
+#include<stdio.h>
+#include<stdlib.h>
+#include<string>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+#pragma warning(disable:4996)
+int main()
+{
+    string num1, num2;
+    size_t height = num2.length();
+    vector<string> cal;
+    while (getline(cin, num1)) {
+        if (cin.eof()) break;
+        getline(cin, num2);
+        
+    }
+    system("pause");
+    return 0;
+}
+
+
+*/
+
